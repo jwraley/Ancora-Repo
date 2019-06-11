816f0762357b11a29777067afb9838413fb87490
steven.schronk <steven.schronk@faculty.ancoraed.com>
2019-06-11 08:00:17 -0500

    Added SQL Homework 2

---
+++ SQLHW2.sql
@@ -0,0 +1,44 @@
+-- Select all cities in the BGR country code and not in the District of England.
+
+-- Select all cities with districts that start with 'al-'.
+
+-- Select all cities with Populations between 1 and 2 million.
+
+-- Select all cities with Populations between 1 and 2 million that are in the EGY country code.
+
+-- Get the total population of the EGY cities.
+
+-- Get the average population of the PHL and HKG cities.
+
+-- Get list of all cities sorted first by the country code in alphabetical order and then by the population from greatest to least.
+
+-- Get a list of the Minimum, Maximum and Average population of each country code ordered by country code.  Rename the columns to something sensible.
+
+-- Get list of all country names that end in  'an' ordered by country code.
+
+-- Get list of all countries ordered by continent and sorted by surface area.
+
+-- Get list of countries with life expectancy > 60 and GNP > 100
+
+-- Get an alphabetical list of countries where the GNP is not the same as the GNPOld
+
+-- Get alphabetical list of country names and  3 letter capitol code.
+
+-- Get list of all government forms in the country language table listed in alphabetical order; determine the total land area of each.
+
+-- Get list of all un-official languages in each country code ordered by it's percentage of usage.
+
+-- Get list of languages that are not officially in the EU; join this table to the country table by country code and then add the population and GNP.
+
+-- Get list of countries ordered by independance year; rename column to Independance.
+
+-- Get a list of countries who's name is not the same as it's local name; order table by population.
+
+-- Find list of continents that have Republic governments; order them by continent.
+
+-- Find all heads of state that have characters outisde of [A-Z] and [a-z].
+
+-- Find the sum of all contenent surface area grouped by continent and ordered from greatest to least.
+
+-- Get a list of each form of government that does not have an Independance day; make sure there are no repeated forms of government; get a count of countries that have each form.
+

31e6d797267d39961ed142c4ea684a7d03dc6cf6
Jeffrey Raley <jraley@my.ancoraed.com>
2019-06-14 11:41:57 -0500

    added database schema to begin planning website

---
+++ database schema.sql
@@ -0,0 +1,21 @@
+Purpose: (paragraph describing web site)
+
+Name: (unoffensive, short, memorable, and available domain netcheap.com)
+
+Schema:
+
+	5 tables (min)
+	3 columns or more of each
+
+Tablename:
+	Users  Varchar 30 NotNull
+
+
+Datatypes:
+
+	Varchar:	variable lenght strings
+	date:		Date
+	DateTime:	Date and Time
+	Boolean:	Yes and No
+	Int:		whole numbers
+	Money:		cha ching
